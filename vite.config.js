5d26a9648b1828c84337b755dbb6d34087e045c4
Anibal Morales <anibal.ingenieria@gmail.com>
2025-02-22 17:07:29 -0600

    CORRECCIONES

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 
-dotenv.config();
+export default defineConfig(({ mode }) => {
+  console.log("Entorno de ejecución:", mode);
+  console.log("VITE_INSTANCE desde process.env:", process.env.VITE_INSTANCE);
+  console.log("VITE_API_URL desde process.env:", process.env.VITE_API_URL);
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    "import.meta.env.VITE_INSTANCE": JSON.stringify(process.env.VITE_INSTANCE || "default-instance"),
-    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL || "https://api.default.com"),
-  },
+  return {
+    plugins: [react()],
+    define: {
+      "import.meta.env.VITE_INSTANCE": JSON.stringify(process.env.VITE_INSTANCE || "default-instance"),
+      "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL || "https://api.default.com"),
+    },
+  };
 });
